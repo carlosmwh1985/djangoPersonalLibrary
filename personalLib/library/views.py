f71c0b10a218a08f4d8992204a5cca6ea3ff334d
Carlos Granados <carlosm.granadosc@gmail.com>
2022-09-12 00:56:35 +0200

    First working view of BookList+BookDetail

--- personalLib/library/views.py
+++ personalLib/library/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from django.views import generic
+from django.utils import timezone
 
-# Create your views here.
+from .models import Author, Editorial, Book
+
+class IndexView(generic.ListView):
+    template_name = 'library/index.html'
+    context_object_name = 'book_list'
+
+    def get_queryset(self):
+        """Return..."""
+        return Book.objects.order_by('libro_id')
+
+
+class DetailView(generic.DetailView):
+    template_name: str = 'library/detail.html'
+    model = Book
+
