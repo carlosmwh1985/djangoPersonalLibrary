0dad482ab9efdd31a77fdd3d792a8810861345f1
Carlos Granados <carlosm.granadosc@gmail.com>
2022-09-11 20:00:37 +0200

    Admin Site Implemented

--- personalLib/library/admin.py
+++ personalLib/library/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Author, Editorial, Book
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    fields = ['nombre', 'apellido', 'nacionalidad']
+    list_display = ('id', 'nombre', 'apellido', 'nacionalidad')
+    list_filter = ['nacionalidad']
+    search_fields = ['nombre', 'apellido']
+
+
+class BookAdmin(admin.ModelAdmin):
+    fields = ['libro_id', 'autor', 'editorial', 'isbn', 'leido']
+    # fieldsets = [
+    #     ('Ids', {'fields': ['id', 'libro_id', 'autor_id', 'editorial_id']}),
+    #     ('Book Info', {'fields': ['titulo', 'pags', 'isbn', 'ejemplares']}),
+    #     ('Others', {'fields': ['leido', 'observaciones']})
+    # ]
+    list_display = ('id', 'libro_id', 'titulo', 'isbn', 'leido')
+    list_filter = ('autor', 'editorial', 'leido')
+    search_fields = ['titulo', 'autor_id', 'editorial_id']
+
+
+admin.site.register(Author, AuthorAdmin)
+admin.site.register(Book, BookAdmin)
--- personalLib/library/models.py
+++ personalLib/library/models.py
@@ -1,24 +1,43 @@
 from django.db import models
 
 
-class TblAutores(models.Model):
+class Author(models.Model):
     nombre = models.CharField(max_length=255)
     apellido = models.CharField(max_length=255)
     nacionalidad = models.CharField(max_length=100)
 
+    def __str__(self):
+        return '{}, {}'.format(self.apellido, self.nombre)
 
-class TblEditoriales(models.Model):
+    class Meta:
+        db_table = 'tblautores'
+
+
+class Editorial(models.Model):
     nombre = models.CharField(max_length=255)
 
+    def __str__(self):
+        return self.nombre
 
-class TblLibros(models.Model):
+    class Meta:
+        db_table = 'tbleditoriales'
+
+
+
+class Book(models.Model):
     libro_id = models.CharField(max_length=20)
     titulo = models.TextField()
-    autor_id = models.ForeignKey(TblAutores, on_delete=models.CASCADE)
-    editorial_id = models.ForeignKey(TblEditoriales, on_delete=models.CASCADE)
+    autor = models.ForeignKey(Author, on_delete=models.CASCADE)
+    editorial = models.ForeignKey(Editorial, on_delete=models.CASCADE)
     pags = models.IntegerField(default=0)
     isbn = models.CharField(max_length=21)
     ejemplares = models.IntegerField(default=1)
     leido = models.BooleanField(default=False)
     observaciones = models.TextField()
+
+    def __str__(self):
+        return '{}, \"{}\", by {}'.format(self.libro_id, self.titulo, self.autor.nombre)
+
+    class Meta:
+        db_table = 'tbllibros'
 
